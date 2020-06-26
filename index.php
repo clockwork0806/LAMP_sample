0fb34bbb9b300801ddb36f84e3ca542974f15720
clockwork0806 <yuya.kikuchi.sys19@morijyobi.ac.jp>
2020-06-26 12:39:04 +0900

    add index.php

--- index.php
+++ index.php
@@ -86,7 +86,26 @@ try {
             </tbody>
            
             </table></div>
-        <div class="tab-pane" id="insert"><p>sample tab2</p></div>
+        <div class="tab-pane" id="insert">
+            <form class="mt-3" action="./insert.php" method="POST">
+            <div class="form-group row">
+                <label for="id" class="col-sm-2 col-   form-label">ID</label>
+                <div class="col-sm-10">
+        <input type="text" class="form-control"  id="id"name="id">
+        </div></div>
+                <div class="form-group row">
+                <label for="name" class="col-sm-2 col-   form-label">Your Name</label>
+                <div class="col-sm-10">
+        <input type="text" class="form-control"  id="name"name="name">
+        </div></div>
+                    <div class="form-group row">
+                <label for="age" class="col-sm-2 col-   form-label">Age</label>
+                <div class="col-sm-10">
+        <input type="text" class="form-control"  id="age"name="age">
+        </div>
+                </div>
+            </form>
+            </div>
         <div class="tab-pane" id="update"><p>sample tab3</p></div>
         <div class="tab-pane" id="delete"><p>sample tab4</p></div>
     </div>
